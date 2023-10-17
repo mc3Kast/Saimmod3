c594c3652c78eed8313d9d5b0d04f28a1bb29960
mc3Kast <k3sha2014@yandex.ru>
2023-10-17 23:58:17 +0300

    Добавьте файлы проекта.

---
+++ Math3/Lib/Element.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math3.Lib
+{
+    public class Element
+    {
+        public static Action<Element, Element, Vocation> OnVocationCreated;
+        public static Action<Element, Element> OnBlockPrevious;
+
+        protected Element sender;
+        protected Element reciever;
+        protected TraficCounter counter;
+        protected Vocation vocation;
+
+        protected bool isBusy = false;
+
+
+        public Vocation Vocation
+        {
+            get
+            {
+                return vocation;
+            }
+        }
+
+
+        public virtual TraficCounter TraficCounter
+        {
+            get
+            {
+                return counter;
+            }
+        }
+
+
+        public virtual bool IsBusy
+        {
+            get
+            {
+                return isBusy;
+            }
+        }
+
+
+        public virtual string State
+        {
+            get
+            {
+                return "";
+            }
+        }
+
+
+        public bool IsProcessed
+        {
+            get; set;
+        }
+
+
+        public virtual void ProcessTick(bool isFreeNextElement)
+        {
+
+        }
+
+
+        public virtual void VocationLiveTimeIncrement()
+        {
+            if (vocation != null)
+            {
+                vocation.Increment();
+            }
+        }
+
+
+        public virtual void Init(Element sender, Element reciever, TraficCounter counter = null, Manager manager = null)
+        {
+            IsProcessed = false;
+            this.sender = sender;
+            this.reciever = reciever;
+            this.counter = counter;
+        }
+    }
+}
